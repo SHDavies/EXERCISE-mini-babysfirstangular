bc946e21055c765ba066436c6ed243abb9cb3c1e
Spencer Davies <spencerhdavies@gmail.com>
2015-07-20 14:44:22 -0600

    finished extra feature

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,9 @@
 var app = angular.module('friendsList');
 app.controller('mainCtrl', function($scope) {
 	$scope.friends = ['Holly', 'Joseph', 'Adam', 'Myself'];
+
+	$scope.addFriend = function() {
+		$scope.friends.push($scope.friendInput);
+		$scope.friendInput = '';
+	};
 });
